c30538cc19b82bbac9e5aa26f04cb1e1d7502072
User <justsuvorov@gmail.com>
2023-02-22 14:15:42 +0300

    Выбор ДО и месторождений из списка

---
+++ src/Program/DomainModel/FieldDO.py
@@ -0,0 +1,37 @@
+from Program.BaseObject.BaseObject import BaseObject
+from Program.BaseObject.Sensor import Sensor
+from Program.BaseObject.ObjectInfo import ObjectInfo
+from Program.BaseObject.Indicators import Indicators
+from Program.Well.WellInfo import WellInfo
+from Program.DomainModel import DomainModel
+
+
+class FieldDO(BaseObject):
+    def __init__(self,
+                 name: str,
+                 object_info: ObjectInfo,
+                 indicators: Indicators,
+                 sensor: Sensor,
+                 link: dict = None,
+                 field_info: WellInfo = None
+                 ) -> None:
+            super().__init__(
+                 name=name,
+                 object_info=object_info,
+                 indicators=indicators,
+                 link=link,
+                 sensor=sensor,
+            )
+            self.cluster_info = field_info
+
+    def build(self):
+        pass
+    def data(self):
+        pass
+
+    @classmethod
+    def from_data(cls, domain_model: DomainModel, name: str):
+        try:
+            return domain_model.wellDO(name)
+        except:
+            print('No data for this well')
