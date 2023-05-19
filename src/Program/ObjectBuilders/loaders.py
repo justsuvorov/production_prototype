1e83a122a9648dd63d4b84caea4660b51ea66acf
User <justsuvorov@gmail.com>
2023-05-19 15:41:56 +0300

    SQL SPEAKING and ARO MOnitoring

---
+++ src/Program/ObjectBuilders/loaders.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import sqlite3
+from abc import ABC
+
+
+
+class Loader(ABC):
+    def __init__(self,
+                 data: pd.DataFrame,
+                 source_path: str,
+                 ):
+        self.data = data
+        self.source_path = source_path
+
+    def load_data(self):
+        pass
+
+
+class BlackListLoaderExcel(Loader):
+    def __init__(self,
+                 data: pd.DataFrame,
+                 source_path: str,
+                 ):
+        super().__init__(data=data,
+                         source_path=source_path)
+
+    def load_data(self):
+        self.data.to_excel(self.source_path+'\Black_list.xlsx')
+        print('Результаты записаны в Excel')
+
+
+class BlackListLoaderDB(Loader):
+    def __init__(self,
+                 data: pd.DataFrame,
+                 source_path: str,
+                 ):
+        super().__init__(data=data,
+                         source_path=source_path)
+        self.initial_names = ['id', 'obj_type', 'well_name', 'well_group_name', 'preparation_obj_name',
+                              'field_name', 'company_name', 'date_creation', 'status']
+
+    def load_data(self):
+
+        engine = sqlite3.connect(self.source_path+'\monitoring.db')
+        self.data.columns = self.initial_names
+        self.data.to_sql('monitoring_unprofit_obj', con=engine, if_exists='replace', index=False)
+        print('BlackListLoaderDB||Результаты записаны в Базу данных')
+
+
+class AROFullLoaderDB(Loader):
+    def __init__(self,
+                 data: pd.DataFrame,
+                 source_path: str,
+                 ):
+        super().__init__(data=data,
+                         source_path=source_path)
+        self.initial_names = ['object_id',
+                                'npv_max',
+                                'fcf_first_month',
+                             #   'oil_production_first_month',
+                                'oil_production_full',
+                                'fluid_extraction_full',
+                                'fcf_full',
+                                'oil_production_gap',
+                                'fluid_extraction_gap',
+                                'fcf_gap',
+                                'calculation_horizon',
+                                'oil_production_year',
+                                'fluid_extraction_year',
+                                'fcf_year',
+                              ]
+
+    def load_data(self):
+
+        self.data.columns = self.initial_names
+        engine = sqlite3.connect(self.source_path+'\monitoring.db')
+     #   for row in self.data.iterrows():
+
+        self.data.to_sql('monitoring_ecm_prod_full', con=engine, if_exists='replace', index=False)
+        print('AROFullLoaderDB||Результаты записаны в Базу данных')
+
+
+class ActivityLoaderDB(Loader):
+    def __init__(self,
+                 data: pd.DataFrame,
+                 source_path: str,
+                 ):
+        super().__init__(data=data,
+                         source_path=source_path)
+        self.initial_names = ['object_id', 'activity_id', 'activity_comment', 'date_planning', 'date_fact',
+                              'responsible_person', 'obj_status', 'date_creation', ]
+
+    def load_data(self):
+
+        engine = sqlite3.connect(self.source_path+'\monitoring.db')
+        self.data.columns = self.initial_names
+        self.data.to_sql('activity_unprofit', con=engine, if_exists='replace', index=False)
+        print('ActivityLoaderDB||Результаты записаны в Базу данных')
+
+
+class AROMonthTableLoaderDB(Loader):
+
+    def __init__(self,
+                 data: pd.DataFrame,
+                 source_path: str,
+                 ):
+        super().__init__(data=data,
+                         source_path=source_path)
---
+++ src/Program/ObjectBuilders/sql_speaking_objects.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import sqlite3
+from Program.ObjectBuilders.Parser import *
+from Program.ObjectBuilders.loaders import *
+
+class SQLSpeakingObject:
+    def __init__(self,
+                 db_name: str,
+                 ):
+        self.db_name = db_name
+        self.cursor = None
+        self.connection = None
+        try:
+            self.connection = sqlite3.connect(self.db_name)
+            self.cursor = self.connection.cursor()
+            print('Connected to ', db_name)
+        #  connection.close()
+        except:
+            print('Unable to connect to db', self.db_name)
+
+
+class GfemSQLSpeakingObject(SQLSpeakingObject):
+    def __init__(self,
+                 path: str,
+                 add_data_from_excel: bool = False
+                 ):
+        self.path = path
+        self.db_name = self.path + '\gfem_results.db'
+        super().__init__(db_name=self.db_name)
+        self.__gfem_base_parser = GfemDataBaseParser(data_path=self.db_name,
+                                                     add_data_from_excel=add_data_from_excel,
+                                                     file_path=self.path)
+
+    def data(self):
+        return self.__gfem_base_parser.data()
+
+    def transfer_month_table(self):
+        self.__gfem_base_parser.transfer_month_table(path=self.path)
+
+
+class MonitoringSQLSpeakingObject(SQLSpeakingObject):
+    def __init__(self,
+                 path: str, ):
+        self.path = path
+        self.db_name = self.path + '\monitoring.db'
+        super().__init__(db_name=self.db_name)
+
+        self.__monitoring_base_parser = MonitoringBaseParser(data_path=self.db_name)
+        self.__monitoring_full_parser = MonitoringFullParser(data_path=self.db_name)
+        self.__monitoring_activity_parser = MonitoringActivityParser(data_path=self.db_name)
+
+    def black_list_from_db(self):
+        return self.__monitoring_base_parser.data()
+
+    def full_data_black_list_from_db(self):
+        return self.__monitoring_full_parser.data()
+
+    def activity_data_from_db(self):
+        return self.__monitoring_activity_parser.data()
+
+    def load_activity_data_to_db(self, data):
+        ActivityLoaderDB(data=data, source_path=self.path).load_data()
+
+    def load_black_list_to_db(self, data: pd.DataFrame):
+        BlackListLoaderDB(data=data, source_path=self.path).load_data()
+
+    def load_full_data_to_db(self, data: pd.DataFrame):
+        AROFullLoaderDB(data=data, source_path=self.path).load_data()
+
+    def insert(self):
+        if self.df is None:
+            print('No data to insert')
+        else:
+            for raw in self.df.iterrows():
+                self.cursor.execute('''INSERT OR IGNORE''')
+
+    def delete_inactive(self):
+        self.cursor.execute('''DELETE FROM monitoring_ecm_prod_full WHERE object_id NOT IN (SELECT id FROM monitoring_unprofit_obj)''')
+        self.cursor.execute(
+            '''DELETE FROM monitoring_ecm_prod_monthly WHERE object_id NOT IN (SELECT id FROM monitoring_unprofit_obj)''')
+        self.connection.commit()
+
+
+class SQLMorDBSpeakingObject(SQLSpeakingObject):
+    def __init__(self,
+                 path: str,
+                 ):
+        self.path = path
+        self.db_name = self.path + '\mor_db.db'
+        super().__init__(db_name=self.db_name)
+
+    def last_month_active_data(self):
+        self.connection = sqlite3.connect(self.db_name)
+
+        df = pd.read_sql_query('''SELECT * FROM mor_info WHERE id in (SELECT id_parent FROM mor_prod WHERE (date IN (SELECT max(date) FROM mor_prod)) AND (end_month_status = 'РАБ.' OR end_month_status = 'ПЬЕЗ.' )''', self.connection)
+
+
+        return df
+
+    def last_month_inactive_data(self):
+        self.connection = sqlite3.connect(self.db_name)
+        #    df = pd.read_sql_query('SELECT * FROM mor_prod WHERE date IN (SELECT max(date) FROM mor_prod)', self.connection)
+        df = pd.read_sql_query(
+            '''SELECT * FROM mor_info WHERE id in (SELECT id_parent FROM mor_prod WHERE (date IN (SELECT max(date) FROM mor_prod)) AND (end_month_status = 'ОСТ.' OR  end_month_status = 'ЛИК')''',
+            self.connection)
+        # AND end_month_status = РАБ.
+
+        return df
